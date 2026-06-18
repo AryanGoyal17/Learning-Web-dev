0fb8ef6cf896871c1a4b28359227d2352259afbe
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-18 21:09:11 +0530

    deleted duplicate files

--- Front-end Projects/01_Circumference_calculator/index.js
+++
@@ -1,9 +0,0 @@
-let radius;
-const PI = Math.PI;
-
-document.getElementById("submit_r").onclick = function(){
-
-    radius = document.getElementById("input_r").value;
-    circumference = 2 * PI * radius;
-    document.getElementById("answer").textContent = `Circumference(cm): ${circumference}`;
-}
