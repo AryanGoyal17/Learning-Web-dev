884ff31e5fdfcf353e631f6cf237ed7a06051c50
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-06-21 22:59:57 +0530

    Operations in js

---
+++ Frontend/JavaScript/Js-Notes_final/Basics/04_operations.js
@@ -0,0 +1,53 @@
+//finding -ve of a number -- done
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+//Basic math ops -- done
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+//String concatenation -- done
+
+//ops on string + number -- done
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+// priority order -- done
+// console.log(3 + 4 * 5 % 3); // ** > *,/,% > +, - , if priority same go from left to right
+
+//Another method for variable assignment -- done
+num1 = num2 = num3 = 2+2
+console.log(num1, num2, num3)
+
+//Post-increment v/s pre-increment -- done
+
+// x++ -- use it then change it
+// ++x -- change it and then use it 
+//NOTE: if it lies on in a line like x++ or ++x , both do the same thing as adding 1, to show diff they must be assigned to another variable/printed directly etc..
+
+//A single + before a string/boolean(used for typecasting to number) -- done
+
+console.log(+true, +"", +"aryan")
+
+//Notes -- 
+// 1-- -ve of a number
+// 2-- basic math ops
+// 3-- string concatenation
+// 4-- ops on string + number
+// 5-- priority order
+// 6-- Another method for variable assignment
+// 7-- Post-increment v/s pre-increment
+// 8-- Another method for typecasting to number
+
+
+
+
+
